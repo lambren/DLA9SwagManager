fd7b55244f2aeab6aa12e55c5fb38141c73fc2b7
KuhnTran <KuhnTran@gmail.com>
2021-05-23 22:35:39 -0700

    Updates

---
+++ precache-manifest.3bb55b01e9a6312baf298e2831de02e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b330218da908a6f2e093ed3d7d3df55",
+    "url": "/DLA9SwagManager/index.html"
+  },
+  {
+    "revision": "f6b141feb7cb566d8035",
+    "url": "/DLA9SwagManager/static/css/main.990482f4.chunk.css"
+  },
+  {
+    "revision": "aa39552676f25339f8e0",
+    "url": "/DLA9SwagManager/static/js/2.7c84e84e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DLA9SwagManager/static/js/2.7c84e84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b141feb7cb566d8035",
+    "url": "/DLA9SwagManager/static/js/main.25669fab.chunk.js"
+  },
+  {
+    "revision": "ad465941d9fbc3899ab2",
+    "url": "/DLA9SwagManager/static/js/runtime-main.2ffdbb08.js"
+  }
+]);
